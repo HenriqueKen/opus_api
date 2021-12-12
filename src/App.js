fff88b008669cdc6628eefb4d398dc8d25359c4e
Henrique Ken <henriqueken.mochizuki@gmail.com>
2021-12-12 15:16:05 -0300

    Tentar API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,125 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
 
-function App() {
+function MoviesTable(props) {
+  let { list, onDismiss, searchTerm} = props;
+  if(!searchTerm){searchTerm = ""}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <table>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Título</th>
+            <th>Ano</th>
+            <th>URL do Poster</th>
+          </tr>
+        </thead>
+        <tbody>
+          {list.filter(movie => movie.name.toLowerCase().includes(searchTerm.toLowerCase()))
+          .map((movie) => (
+            <tr key={movie.id}>
+              <td>{movie.id}</td>
+              <td>{movie.name}</td>
+              <td>{movie.release_year}</td>
+              <td>{movie.poster_url}</td>
+              <td>
+                <Button onClick={() => onDismiss(movie.id)}>Dismiss</Button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
   );
+}
+
+function Search(props) {
+  const {searchTerm, handleInputChange, children} = props;
+  return (
+      <form>
+        {children} <input type="text" placeholder="Search by movie name"
+          name="searchTerm" value={searchTerm} 
+          onChange={(event) => handleInputChange(event)}/>
+      </form>
+    );
+}
+
+function Button(props) {
+  const {
+    onClick,
+    className='',
+    children,
+  } = props;
+
+  return (
+    <button
+      onClick={onClick}
+      className={className}
+      type="button"
+    >
+      {children}
+    </button>
+  );
+}
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { list: null };
+  }
+
+  onDismiss(id) {
+    const updatedList = this.state.list.filter(item => item.id !== id);
+    this.setState({ list: updatedList });
+  }
+
+  onSearchChange(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = 
+      target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  componentDidMount() {
+    fetch('http://127.0.0.1:8000/api/v1/movies/')
+      .then((response) => response.json())
+      .then((result) => this.setState({ list: result }))
+      .catch((error) => error);
+  }
+
+  render() {
+    const {list, searchTerm} = this.state;
+
+    if (!list) {
+      return null;
+    }
+    
+    return (
+      <div className="App">
+        <Search
+          searchTerm={searchTerm}
+          handleInputChange={(e) => this.handleInputChange(e)}
+        >
+          Search term:
+        </Search>
+        { list ? (
+          <MoviesTable 
+            list={list} 
+            searchTerm={searchTerm}
+            onDismiss={(id) => this.onDismiss(id)}
+          />
+        ) : null}
+      </div>
+    );
+  }
 }
 
 export default App;
